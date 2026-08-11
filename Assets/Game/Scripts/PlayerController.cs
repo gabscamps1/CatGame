674b29aa51d9e502675ab1d13ec40fe7a7caa089
Gabriel de Campos <gabrielultrasinho@gmail.com>
2026-08-11 10:43:28 -0300

    início player input

---
+++ Assets/Game/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private BoxCollider2D _boundaries;
+    [SerializeField] private Transform _fruitThrowTransform;
+
+    private Bounds _bounds;
+
+    private float _leftBound;
+    private float _rightBound;
+
+    private float _startingLeftBound;
+    private float _startingRightBound;
+
+    private float _offset;
+
+    private void Awake()
+    {
+        _bounds = _boundaries.bounds;
+
+        _offset = transform.position.x - _fruitThrowTransform.position.x;
+
+        _leftBound = _bounds.min.x + _offset;
+        _rightBound = _bounds.max.x + _offset;
+
+        _startingLeftBound = _leftBound;
+        _startingRightBound = _rightBound;
+    }
+
+    private void Update()
+    {
+        Vector3 newPosition = transform.position + new Vector3(UserInput.MoveInput.x * _moveSpeed * Time.deltaTime, 0f, 0f);
+        newPosition.x = Mathf.Clamp(newPosition.x, _leftBound, _rightBound);
+
+        transform.position = newPosition;
+    }
+
+}
