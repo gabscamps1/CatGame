82cc0d712ef06f7c94e24eacd75e2694486f77cc
Gabriel de Campos <gabrielultrasinho@gmail.com>
2026-08-11 19:55:42 -0300

    add scripts de fruta
    - spawnar fruta aleatória
    - linha de fruta
    - indicador de próxima fruta

---
+++ Assets/Game/Scripts/ColliderInformation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ColliderInformation : MonoBehaviour
+{
+    public bool WasCombinedIn { get; set; }
+
+    private bool _hasCollided;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!_hasCollided && !WasCombinedIn)
+        {
+            _hasCollided = true;
+            ThrowFruitController.instance.CanThrow = true;
+            ThrowFruitController.instance.SpawnAFruit(FruitSelector.instance.NextFruit);
+            FruitSelector.instance.PickNextFruit();
+            Destroy(this);
+        }
+        
+    }
+    
+}
